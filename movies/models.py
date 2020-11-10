5a796f9d89e9b2e4d4a895fc6c00ce7a786ba37b
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-11-10 10:42:52 -0500

    constructed movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    year = models.CharField(max_length=100)
+    title = models.TextField()
+    origin = models.TextField()
+    director = models.TextField()
+    cast = models.TextField()
+    genre = models.TextField()
+    wikipage = models.TextField()
+    plot = models.TextField()
+
+    def __str__(self):
+        return self.title
