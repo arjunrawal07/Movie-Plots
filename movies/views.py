71942f35d0dabe9010a0d8aabac28dd9ee70dbde
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-11-10 11:13:26 -0500

    added view details for Movie

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import MovieSerializer
+from .models import Movie
 # Create your views here.
+
+
+class MovieList(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
